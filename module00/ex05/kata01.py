e4e311fcd7752457f0b565c8667aac2f0f74c50a
Achraf Belarif <abelarif@c4r1p5.1337.ma>
2023-10-01 15:07:46 +0100

    reset repository

--- module00/ex05/kata01.py
+++
@@ -1,9 +0,0 @@
-# Put this at the top of your kata01.py file
-kata = {
-'Python': 'Guido van Rossum',
-'Ruby': 'Yukihiro Matsumoto',
-'PHP': 'Rasmus Lerdorf',
-}
-
-for key, value in kata.items():
-    print(f"{key} was created by {value}")
--- module00/ex05/kata03.py
+++
@@ -1,4 +0,0 @@
-# Put this at the top of your kata03.py file
-kata = "The right format"
-
-print('-' * (42 - len(kata)) + kata, end="")
