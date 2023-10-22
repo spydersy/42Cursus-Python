c1480951d4160a404a3e3c4458f37999b9b97f0d
spydersy <acbe1325@aol.com>
2023-10-22 23:45:45 +0100

    [DONE]: module0/ex01

--- module00/ex01/exec.py
+++ module00/ex01/exec.py
@@ -1,6 +1,8 @@
 import sys
 
 if len(sys.argv) > 1:
-    rev = sys.argv[1:]
-    rev.reverse()
-    print(rev) 
+    arguments = sys.argv[1::]
+    merged_arguments = ' '.join(arguments)    
+    reversed_arguments = merged_arguments[::-1]
+    swapped_case_string = reversed_arguments.swapcase()
+    print(swapped_case_string)
