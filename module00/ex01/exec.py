a7d91847dca4c207c133176491e2d478623de491
Achraf Belarif <abelarif@e2r1p2.1337.ma>
2023-10-22 22:33:27 +0100

    test generted key

--- module00/ex01/exec.py
+++ module00/ex01/exec.py
@@ -1,7 +1,6 @@
 import sys
 
 if len(sys.argv) > 1:
-    arr = sys.argv[1:]
-    arr.reverse()
-    x = ' '.join(arr)
-    print(x.swapcase())
+    rev = sys.argv[1:]
+    rev.reverse()
+    print(rev)
