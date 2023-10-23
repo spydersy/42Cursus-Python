d0c108d34635884ddd6ae386767654ed93a1fef8
spydersy <acbe1325@aol.com>
2023-10-23 18:17:12 +0100

    [TODO]: complet module00/ex03

---
+++ module00/ex04/operations.py
@@ -0,0 +1,39 @@
+import sys
+
+too_many_arguments_error = 'AssertionError: too many arguments'
+missing_argument_error = 'AssertionError: Missing argument'
+not_a_number_error = 'AssertionError: argument is not an integer'
+division_by_zero_error = 'ERROR (division by zero)'
+module_by_zero_error = 'ERROR (modulo by zero)'
+
+usage_text = '''
+Usage: python operations.py <number1> <number2>
+Example:
+    python operations.py 10 3
+'''
+
+result = '''\
+Sum:        {}
+Difference: {}
+Product:    {}
+Quotient:   {}
+Remainder:  {}
+'''
+
+if len(sys.argv) == 1:
+    print(usage_text)
+elif len(sys.argv) > 3:
+    print(too_many_arguments_error)
+elif len(sys.argv) < 3:
+    print(missing_argument_error)
+else:
+    try:
+        A = int(sys.argv[1])
+        B = int(sys.argv[2])
+        print(result.format(A + B,
+                            A - B,
+                            A * B,
+                            division_by_zero_error if B == 0 else A / B,
+                            module_by_zero_error if B == 0 else A % B))
+    except:
+        print(not_a_number_error)
